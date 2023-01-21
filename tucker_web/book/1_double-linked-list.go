d714b7092fc68cdd2af480f48131eb736889ec2b
jexists <jexists486@gmail.com>
2023-01-21 22:58:17 +0900

    Linked List

---
+++ tucker_web/book/1_double-linked-list.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+type Node[T any] struct {
+	next *Node[T]
+	prev *Node[T]
+	val  T
+}
+
+func main() {
+	root := &Node[int]{nil, nil, 10}
+	root.next = &Node[int]{nil, root, 20}
+	root.next.next = &Node[int]{nil, root.next, 30}
+
+	tail := root.next.next
+	for n := tail; n != nil; n = n.prev {
+		fmt.Printf("node val:%d\n", n.val)
+	}
+}
