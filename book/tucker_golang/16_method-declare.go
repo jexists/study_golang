4e0297e646ec87776235ded9c79a7186be26f94e
jexists <jexists486@gmail.com>
2023-06-01 23:16:46 +0900

    메서드

--- book/tucker_golang/16_method-declare.go
+++ book/tucker_golang/16_method-declare.go
@@ -1,5 +1,27 @@
 package main
 
-func main() {
+import (
+	fmt
+)
 
+type account stuct {
+	balance int
+}
+
+func withdrawFunc(a *account, amount int) { // 일반 함수 표현
+	a.balance -= amount
+}
+
+func (a *account) withdrawFunc(amount int) { // 메서드 표현
+	a.balance -= amount
+}
+
+func main() {
+	a := &account{100}
+
+	withdrawFunc(a, 30) // 함수 형태 호출
+
+	a.withdrawFunc(30) // 메서드 형태 호출
+
+	fmt.Printf("%d \n", a.balance)
 }
